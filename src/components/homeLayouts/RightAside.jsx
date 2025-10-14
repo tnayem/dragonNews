03027af8e079f53a9002aa7ccabd74c8beaef817
tnayem <tasdidnayem@gmail.com>
2025-10-14 21:01:18 +0600

    implement QZone section

--- src/components/homeLayouts/RightAside.jsx
+++ src/components/homeLayouts/RightAside.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
 import FindUs from '../../layouts/HomeLayout/FindUs';
+import QZone from '../../layouts/HomeLayout/QZone';
 
 const RightAside = () => {
     return (
         <div className="space-y-5">
             <SocialLogin></SocialLogin>
             <FindUs></FindUs>
+            <QZone></QZone>
         </div>
     );
 };
