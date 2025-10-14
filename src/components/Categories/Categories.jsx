54a82e38b3dea7297a944de631f58f84fe512a1c
tnayem <tasdidnayem@gmail.com>
2025-10-14 15:25:47 +0600

    left aside section compleate

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,4 +1,6 @@
 import React, { use } from 'react';
+import { NavLink } from 'react-router';
+import './Categories.css'
 const categoriesPromiss = fetch('/categories.json').then(res=>res.json())
 
 const Categories = () => {
@@ -7,6 +9,15 @@ const Categories = () => {
     return (
         <div>
             <h2 className='font-bold'>All Categories {categories.length}</h2>
+            <div className='grid grid-cols-1 mt-5 gap-3'>
+                {
+                    categories?.map(category=><NavLink 
+                        key={category.id}
+                        className={"btn bg-base-100 border-0 hover:bg-gray-300"}
+                        to={`/category/${category.id}`}
+                        >{category.name}</NavLink>)
+                }
+            </div>
         </div>
     );
 };
