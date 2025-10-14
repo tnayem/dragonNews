54fe1e607427bd0f3ae6a2823cee719b84444ccc
tnayem <tasdidnayem@gmail.com>
2025-10-14 11:53:32 +0600

    navbar design compleate

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+import userImg from '../../assets/user.png'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between'>
+           <div></div>
+           <div className='nav flex gap-5'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/career'>Career</NavLink>
+           </div>
+           <div className='login-btn flex gap-2 items-center'>
+            <img src={userImg} alt="" />
+            <button className='btn bg-[#706F6F] text-white px-10'>Login</button>
+           </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/layouts/HomeLayout/HomeLayout.jsx
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../../components/Header/Header';
 import LatestNews from '../../components/LatestNews/LatestNews';
+import Navbar from '../../components/Navbar/Navbar';
 
 const HomeLayout = () => {
     return (
@@ -11,6 +12,9 @@ const HomeLayout = () => {
                 <section className='w-11/12 mx-auto'>
                     <LatestNews></LatestNews>
                 </section>
+                <nav className='w-11/12 mx-auto'>
+                    <Navbar></Navbar>
+                </nav>
             </header>
             <main>
                 <section className="left-nav"></section>
