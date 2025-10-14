2270b83aeb32cbfdbfe016a143167711ffa225d7
tnayem <tasdidnayem@gmail.com>
2025-10-14 10:58:27 +0600

    implement date formate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div className='flex items-center flex-col gap-3'>
+        <div className='flex items-center flex-col gap-2'>
             <img src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
+            <p>{format(new Date(),"EEEE, MMMM d, yyyy")}</p>
         </div>
     );
 };
